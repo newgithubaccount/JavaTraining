94db3639cbc49baba63f7df8340d85ade99939df
swapna <swapnakpearl0@gmail.com>
2018-07-26 05:59:15 +0530

    branch commit

---
+++ src/com/company/swapna/VariablesDemo - fbrnach.java
@@ -0,0 +1,22 @@
+package com.company.swapna;
+
+/**
+ * Created by SWAPNA on 5/27/2017.
+ */
+public class VariablesDemo {
+    byte byteVar=10;
+    short shortvar=14;
+    int intvar=15;
+    long longVar=1000;
+    float floatVar=10.45F;
+    double doublevar=10.45791;
+    char ctr='c';
+    String name="swapna";
+
+    public static void main(String[] rags)
+    {
+        VariablesDemo var=new VariablesDemo();
+        System.out.println(var.byteVar);
+    }
+
+}
